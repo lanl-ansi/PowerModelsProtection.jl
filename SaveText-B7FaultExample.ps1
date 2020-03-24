fa835333bf249d039a9243c568e67685f11cd487
Arthur Barnes <abarnes@lanl.gov>
2020-03-24 06:16:10 -0600

    cleanup unnecessary files

--- SaveText-B7FaultExample.ps1
+++
@@ -1,3 +0,0 @@
-$pw = "C:\Program Files\PowerWorld\Simulator 21\pwrworld.exe"
-
-Start-Process -Wait "$pw" "Pwb2Text.aux"
--- SaveText-B7FaultFlat.ps1
+++
@@ -1,3 +0,0 @@
-$pw = "C:\Program Files\PowerWorld\Simulator 21\pwrworld.exe"
-
-Start-Process -Wait "$pw" "B7FaultExample-Pwb2Text.aux"
