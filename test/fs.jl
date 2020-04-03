bec76d06e603f7b73cc6121921429f794d006082
Arthur Barnes <abarnes@lanl.gov>
2020-04-03 11:10:59 -0600

    use case5 for unit tests, add license for opendss cases

--- test/fs.jl
+++ test/fs.jl
@@ -1,6 +1,6 @@
 @testset "balance fault study" begin
     @testset "7-bus Fault Example" begin
-        data = PM.parse_file("../test/data/trans/B7FaultExample.raw", import_all=true)
+        data = PM.parse_file("../test/data/trans/case5.raw", import_all=true)
 
         # use flat start
         for (i,b) in data["bus"]
@@ -15,23 +15,23 @@
         end
 
         data["fault"] = Dict{String, Any}()
-        data["fault"]["1"] = Dict("bus" => 3, "r" => 0.0001)
+        data["fault"]["1"] = Dict("bus" => 2, "r" => 0.0001)
         study_results = FS.run_fault_study(data, ipopt_solver)
-        result = study_results["3"][1]
+        result = study_results["2"][1]
         solution = result["solution"]
 
         @test result["termination_status"] == LOCALLY_SOLVED
 
         bus = result["solution"]["bus"]["1"]
-        @test isapprox(abs(bus["vr"] + 1im*bus["vi"]), 0.201545; atol = 1e-5)
-
-        bus = result["solution"]["bus"]["2"]
-        @test isapprox(abs(bus["vr"] + 1im*bus["vi"]), 0.200887; atol = 1e-5)
+        @test isapprox(abs(bus["vr"] + 1im*bus["vi"]), 0.0610503; atol = 1e-3)
 
         bus = result["solution"]["bus"]["3"]
-        @test isapprox(abs(bus["vr"] + 1im*bus["vi"]), 0.000405; atol = 1e-5)
+        @test isapprox(abs(bus["vr"] + 1im*bus["vi"]), 0.0279175; atol = 1e-3)
 
-        bus = result["solution"]["bus"]["7"]
-        @test isapprox(abs(bus["vr"] + 1im*bus["vi"]), 0.259611; atol = 1e-5)
+        bus = result["solution"]["bus"]["4"]
+        @test isapprox(abs(bus["vr"] + 1im*bus["vi"]), 0.0529499; atol = 1e-3)
+
+        bus = result["solution"]["bus"]["10"]
+        @test isapprox(abs(bus["vr"] + 1im*bus["vi"]), 0.0645659; atol = 1e-3)
     end
 end
