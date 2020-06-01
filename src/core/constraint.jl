f4e73345563e9f739630619202c0f7b4303928fe
Art Barnes <abarnes@lanl.gov>
2020-06-01 17:24:23 -0600

    test results makes sense

--- src/core/constraint.jl
+++ src/core/constraint.jl
@@ -23,7 +23,7 @@ function constraint_pf_inverter(pm::_PM.AbstractIVRModel, n::Int, i, bus_id, pg,
     
     JuMP.@NLconstraint(pm.model, kg*pg == vr*crg + vi*cig)
     JuMP.@NLconstraint(pm.model, kg*qg == vi*crg - vr*cig)
-    # JuMP.@NLconstraint(pm.model, cmax^2 >= crg^2 + cig^2) 
+    JuMP.@NLconstraint(pm.model, cmax^2 >= crg^2 + cig^2) 
 end
 
 ""
