342085a1b2580d84cc8bdb5aaad6cda10ed55205
Jose Enrique Tabarez <jtabarez@lanl.gov>
2020-11-02 12:22:44 -0700

    Update opendss.jl

--- src/io/opendss.jl
+++ src/io/opendss.jl
@@ -1,4 +1,4 @@
-""
+"Parse opendss data file using PMD and math model"
 function parse_opendss(file::String; kwargs...)
     pm_data = _PMD.parse_file(file; data_model = _PMD.MATHEMATICAL, kwargs...)
     pm_data["method"] = "PMD"
