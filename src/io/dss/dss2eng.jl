7caf8e1ee9dafa846028a9354159d351db76eaf8
matthew-job <85261863+matthew-job@users.noreply.github.com>
2021-07-23 10:03:19 -0500

    protection equipment from dss file

--- src/io/dss/dss2eng.jl
+++ src/io/dss/dss2eng.jl
@@ -39,7 +39,7 @@ function _dss2eng_gen_dynamics!(data_eng::Dict{String,<:Any}, data_dss::Dict{Str
 end
 
 
-"helper function to convert dss data for monitors to engineering current transformer model"
+"Helper function to convert dss data for monitors to engineering current transformer model."
 function _dss2eng_ct!(data_eng::Dict{String,<:Any}, data_dss::Dict{String,<:Any})
     for (id, dss_obj) in get(data_dss, "monitor", Dict())
         if haskey(dss_obj, "turns")
@@ -56,13 +56,14 @@ function _dss2eng_ct!(data_eng::Dict{String,<:Any}, data_dss::Dict{String,<:Any}
 end
 
 
-"helper function for converting dss relay to engineering relay model"
+"Helper function for converting dss relay to engineering relay model."
 function _dss2eng_relay!(data_eng::Dict{String,<:Any}, data_dss::Dict{String,<:Any})
     for (id, dss_obj) in get(data_dss, "relay", Dict())
         if haskey(dss_obj, "basefreq") && dss_obj["basefreq"] != data_eng["settings"]["base_frequency"]
             @warn "basefreq=$(dss_obj["basefreq"]) on line.$id does not match circuit basefreq=$(data_eng["settings"]["base_frequency"])"
         end
-        relay_type = dss_obj["type"]
+        add_relay! = true
+        relay_type = strip(dss_obj["type"])
         phase = [1,2,3]
         t_breaker = 0
         shots = 1
@@ -77,103 +78,303 @@ function _dss2eng_relay!(data_eng::Dict{String,<:Any}, data_dss::Dict{String,<:A
             shots = parse(Float64, dss_obj["shots"])
         end
         if haskey(dss_obj, "phase")
-            phases = split(dss_obj["phase"], ',')
-            N = length(phases)
-            phase = zeros(Int, N)
-            for i = 1:N
-                if i == 1
-                    phase[i] = parse(Int64, strip(split(strip(phases[i]), '[')[end]))
-                elseif i < N
-                    phase[i] = parse(Int64, strip(phases[i]))
-                else
-                    phase[i] = parse(Int64, strip(split(strip(phases[i]), ']')[begin]))
-                end
+            phase = strip(dss_obj["phase"])
+            if startswith(phase,'[') && endswith(phase,']')
+                phases = split(split(split(phase, '[')[end],']')[begin],',')
+            elseif startswith(phase,''') && endswith(phase,''')
+                phases = split(split(split(phase, ''')[end],''')[begin],',')
+            elseif startswith(phase,'"') && endswith(phase,'"')
+                phases = split(split(split(phase, '"')[end],'"')[begin],',')
+            end
+            phase = []
+            for i = 1:length(phases)
+                push!(phase,parse(Int,phases[i]))
             end
         end
         element = "<none>"
         if haskey(dss_obj, "element")
-            element = dss_obj["element"]
+            element = strip(dss_obj["element"])
         elseif haskey(dss_obj, "element1")
-            element = dss_obj["element1"]
+            element = strip(dss_obj["element1"])
         elseif haskey(dss_obj, "monitoredobj")
-            element = dss_obj["monitoredobj"]
+            element = strip(dss_obj["monitoredobj"])
         else
-            @warn "Relay $id does not have a monitored object."
+            @warn "Relay $id does not have a monitored object. Could not add relay."
+            add_relay! = false
         end
         if haskey(dss_obj, "phasetrip")
             TS = parse(Float64, dss_obj["phasetrip"])
-        else
+        elseif haskey(dss_obj,"ts")
             TS = parse(Float64, dss_obj["ts"])
+        elseif haskey(dss_obj, "trip_angle")
+            trip_angle = parse(Float64, dss_obj["trip_angle"])
+        else
+            @warn "Relay $id has no tap setting. Could not add relay."
+            add_relay! = false
         end
         if haskey(dss_obj, "tdphase")
             TDS = parse(Float64, dss_obj["tdphase"])
-        else
+        elseif haskey(dss_obj, "tds")
             TDS = parse(Float64, dss_obj["tds"])
+        elseif haskey(dss_obj,"trip_angle")
+        else
+            @warn "Relay $id has no time dial setting. Could not add relay."
+            add_relay! = false
         end
-        if relay_type == "overcurrent"
-            if haskey(dss_obj, "ct") || haskey(dss_obj, "cts")
-                if haskey(dss_obj, "cts")
-                    ct = dss_obj["cts"]
-                else
-                    ct = dss_obj["ct"]
-                end
-                add_relay(data_eng, element, "$id", TS, TDS, ct;phase=phase,t_breaker=t_breaker,shots=shots)
-            else
-                add_relay(data_eng, element, "$id", TS, TDS;phase=phase,t_breaker=t_breaker,shots=shots)
-            end
-        elseif relay_type == "differential"
-            if haskey(dss_obj, "cts")
-                ct_vec = split(dss_obj["cts"], ',')
-            else
-                ct_vec = split(dss_obj["ct"], ',')
-            end
-            N = length(ct_vec)
-            for i = 1:N
-                if i == 1
-                    ct_vec[i] = split(strip(ct_vec[i]), '[')[end]
-                elseif i < N
-                    ct_vec[i] = strip(ct_vec[i])
-                else
-                    ct_vec[i] = split(strip(ct_vec[i]), ']')[begin]
-                end
-            end
-            if haskey(dss_obj, "element2") || haskey(dss_obj, "monitoredobj2")
-                element2 = "<none>"
-                if haskey(dss_obj, "element2")
-                    element2 = dss_obj["element2"]
-                elseif haskey(dss_obj, "monitoredobj2")
-                    element2 = dss_obj["monitoredobj2"]
-                else
-                    @warn "Relay $id does not have a monitored object."
-                end
-                add_relay(data_eng, element, element2, "$id", TS, TDS, ct_vec;phase=phase,t_breaker=t_breaker)
-            else
-                add_relay(data_eng, element, "$id", TS, TDS, ct_vec;phase=phase,t_breaker=t_breaker)
-            end
-        elseif relay_type == "differential_dir"
+        if relay_type == "differential_dir"
             element2 = "<none>"
             if haskey(dss_obj, "element2")
-                element2 = dss_obj["element2"]
+                element2 = strip(dss_obj["element2"])
             elseif haskey(dss_obj, "monitoredobj2")
-                element2 = dss_obj["monitoredobj2"]
+                element2 = strip(dss_obj["monitoredobj2"])
             else
-                @warn "Relay $id does not have a monitored object."
+                @warn "Relay $id needs a second monitored object. Could not add relay."
+                add_relay! = false
             end
-            add_relay(data_eng, element, element2, "$id", parse(Float64, dss_obj["trip_angle"]))
+        end
+        if add_relay!
+            if relay_type == "overcurrent"
+                if haskey(dss_obj, "ct") || haskey(dss_obj, "cts")
+                    if haskey(dss_obj, "cts")
+                        ct = strip(dss_obj["cts"])
+                    else
+                        ct = strip(dss_obj["ct"])
+                    end
+                    add_relay(data_eng, element, "$id", TS, TDS, ct;phase=phase,t_breaker=t_breaker,shots=shots)
+                else
+                    add_relay(data_eng, element, "$id", TS, TDS;phase=phase,t_breaker=t_breaker,shots=shots)
+                end
+            elseif relay_type == "differential"
+                if haskey(dss_obj, "cts")
+                    ct_vec = strip(dss_obj["cts"])
+                else
+                    ct_vec = strip(dss_obj["ct"])
+                end
+                if occursin(',',ct_vec)
+                    if startswith(ct_vec,'[') && endswith(ct_vec,']')
+                        cts = split(split(split(ct_vec, '[')[end],']')[begin],',')
+                    elseif startswith(ct_vec,''') && endswith(ct_vec,''')
+                        cts = split(split(split(ct_vec, ''')[end],''')[begin],',')
+                    elseif startswith(ct_vec,'"') && endswith(ct_vec,'"')
+                        cts = split(split(split(ct_vec, '"')[end],'"')[begin],',')
+                    end
+                    ct_vec = String[]
+                    for i = 1:length(cts)
+                        push!(ct_vec, String(cts[i]))
+                    end
+                end
+                if haskey(dss_obj, "element2") || haskey(dss_obj, "monitoredobj2")
+                    element2 = "<none>"
+                    if haskey(dss_obj, "element2")
+                        element2 = strip(dss_obj["element2"])
+                    elseif haskey(dss_obj, "monitoredobj2")
+                        element2 = strip(dss_obj["monitoredobj2"])
+                    end
+                    add_relay(data_eng, element, element2, "$id", TS, TDS, ct_vec;phase=phase,t_breaker=t_breaker)
+                else
+                    add_relay(data_eng, element, "$id", TS, TDS, ct_vec;phase=phase,t_breaker=t_breaker)
+                end
+            elseif relay_type == "differential_dir"
+                add_relay(data_eng, element, element2, "$id", trip_angle)
+            end
         end
     end
-end    
-    
+end   
 
+
+"Helper function for converting dss fuse to engineering fuse"
 function _dss2eng_fuse!(data_eng::Dict{String,<:Any}, data_dss::Dict{String,<:Any})
     for (id, dss_obj) in get(data_dss, "fuse", Dict())
         if haskey(dss_obj, "basefreq") && dss_obj["basefreq"] != data_eng["settings"]["base_frequency"]
             @warn "basefreq=$(dss_obj["basefreq"]) on line.$id does not match circuit basefreq=$(data_eng["settings"]["base_frequency"])"
         end
+        phase = [1,2,3]
+        element = "<none>"
+        add_fuse! = true
+        if haskey(dss_obj, "element")
+            element = dss_obj["element"]
+        elseif haskey(dss_obj, "monitoredobj")
+            element = dss_obj["monitoredobj"]
+        elseif haskey(dss_obj, "element1")
+            element = dss_obj["element1"]
+        else
+            @warn "Fuse $id has no monitored object."
+            add_fuse! = false
+        end
+        if haskey(dss_obj,"min_melt_curve")
+            min_melt_curve = strip(dss_obj["min_melt_curve"])
+        elseif haskey(dss_obj,"fuse_curve")
+            min_melt_curve = strip(dss_obj["fuse_curve"])
+        elseif haskey(dss_obj,"max_clear_curve")
+            min_melt_curve = strip(dss_obj["max_clear_curve"])
+        else
+            @warn "Fuse $id hase no fuse curve."
+            add_fuse! = false
+        end
+        if haskey(dss_obj,"max_clear_curve")
+            max_clear_curve = strip(dss_obj["max_clear_curve"])
+        else
+            max_clear_curve = min_melt_curve
+        end
+        if haskey(dss_obj, "phase")
+            phase = strip(dss_obj["phase"])
+            if startswith(phase,'[') && endswith(phase,']')
+                phases = split(split(split(phase, '[')[end],']')[begin],',')
+            elseif startswith(phase,''') && endswith(phase,''')
+                phases = split(split(split(phase, ''')[end],''')[begin],',')
+            elseif startswith(phase,'"') && endswith(phase,'"')
+                phases = split(split(split(phase, '"')[end],'"')[begin],',')
+            end
+            phase = []
+            for i = 1:length(phases)
+                push!(phase,parse(Int,phases[i]))
+            end
+        end
+        if add_fuse!
+            if ((startswith(min_melt_curve,'[') && endswith(min_melt_curve,']'))||(startswith(min_melt_curve,''') && endswith(min_melt_curve,'''))
+                ||(startswith(min_melt_curve,'"') && endswith(min_melt_curve,'"')))
+                if startswith(min_melt_curve, '[')
+                    (c_array, t_array) = split(split(split(min_melt_curve,']')[begin],'[')[end],';')
+                elseif startswith(min_melt_curve, ''')
+                    (c_array, t_array) = split(split(split(min_melt_curve,''')[begin],''')[end],';')
+                elseif startswith(min_melt_curve,'"')
+                    (c_array, t_array) = split(split(split(min_melt_curve,'"')[begin],'"')[end],';')
+                end
+                c_array = split(c_array,',')
+                t_array = split(t_array,',')
+                min_melt_curve = zeros(2,length(c_array))
+                if length(c_array) != length(t_array)
+                    c_len, t_len = length(c_array), length(t_array)
+                    c_array, t_array = parse.(Float64,c_array),parse.(Float64,t_array)
+                    if c_len > t_len
+                        @warn "t_array is shorter than c_array. Adding time values."
+                        for i=0:c_len - t_len - 1
+                            push!(t_array, t_array[t_len+i]/2)
+                        end
+                    else
+                        @warn "c_array is shorter than t_array. Adding current values."
+                        c_len = length(c_array)
+                        (a,b) = _bisection(c_array[c_len],t_array[c_len],c_array[c_len-1],t_array[c_len-1])
+                        for i=1:npts - c_len
+                            push!(c_array, round((a/t_array[c_len+i]+1)^(1/b)))
+                        end
+                    end
+                    min_melt_curve[1,:],min_melt_curve[2,:] = parse.(Float64,c_array),parse.(Float64,t_array)
+                else
+                    min_melt_curve[1,:],min_melt_curve[2,:] = parse.(Float64,c_array),parse.(Float64,t_array)
+                end
+            end
+            if ((startswith(max_clear_curve,'[') && endswith(max_clear_curve,']'))||(startswith(max_clear_curve,''') && endswith(max_clear_curve,'''))
+                ||(startswith(max_clear_curve,'"') && endswith(max_clear_curve,'"')))
+                if startswith(max_clear_curve, '[')
+                    (c_array, t_array) = split(split(split(max_clear_curve,']')[begin],'[')[end],';')
+                elseif startswith(max_clear_curve, ''')
+                    (c_array, t_array) = split(split(split(max_clear_curve,''')[begin],''')[end],';')
+                elseif startswith(max_clear_curve,'"')
+                    (c_array, t_array) = split(split(split(max_clear_curve,'"')[begin],'"')[end],';')
+                end
+                c_array = split(c_array,',')
+                t_array = split(t_array,',')
+                max_clear_curve = zeros(2,length(c_array))
+                if length(c_array) != length(t_array)
+                    c_len, t_len = length(c_array), length(t_array)
+                    c_array, t_array = parse.(Float64,c_array),parse.(Float64,t_array)
+                    if c_len > t_len
+                        @warn "t_array is shorter than c_array. Adding time values."
+                        for i=0:c_len - t_len - 1
+                            push!(t_array, t_array[t_len+i]/2)
+                        end
+                    else
+                        @warn "c_array is shorter than t_array. Adding current values."
+                        c_len = length(c_array)
+                        (a,b) = _bisection(c_array[c_len],t_array[c_len],c_array[c_len-1],t_array[c_len-1])
+                        for i=1:npts - c_len
+                            push!(c_array, round((a/t_array[c_len+i]+1)^(1/b)))
+                        end
+                    end
+                    max_clear_curve[1,:],max_clear_curve[2,:] = parse.(Float64,c_array),parse.(Float64,t_array)
+                else
+                    max_clear_curve[1,:],max_clear_curve[2,:] = parse.(Float64,c_array),parse.(Float64,t_array)
+                end
+            end
+            add_fuse(data_eng,"$element","$id",min_melt_curve;max_clear_curve=max_clear_curve,phase=phase)
+        end
     end
 end
 
 
+"Helper function for converting dss tcc_curves to engineering model"
 function _dss2eng_curve!(data_eng::Dict{String,<:Any}, data_dss::Dict{String,<:Any})
-    for (id, dss_obj) in get(data_dss, "curve", Dict())
+    for (id, dss_obj) in get(data_dss, "tcc_curve", Dict())
+        if startswith(strip(dss_obj["c_array"]),'[')
+        c_string = split(split(split(strip(dss_obj["c_array"]),'[')[end],']')[begin],',')
+        elseif startswith(strip(dss_obj["c_array"]),'"')
+        c_string = split(split(split(strip(dss_obj["c_array"]),'"')[end],'"')[begin],',')
+        elseif startswith(strip(dss_obj["c_array"]),''')
+        c_string = split(split(split(strip(dss_obj["c_array"]),''')[end],''')[begin],',')
+        end
+        if startswith(strip(dss_obj["t_array"]),'[')
+        t_string = split(split(split(strip(dss_obj["t_array"]),'[')[end],']')[begin],',')    
+        elseif startswith(strip(dss_obj["t_array"]),'"')
+        t_string = split(split(split(strip(dss_obj["t_array"]),'"')[end],'"')[begin],',')
+        elseif startswith(strip(dss_obj["t_array"]),''')
+        t_string = split(split(split(strip(dss_obj["t_array"]),''')[end],''')[begin],',')
+        end
+        c_array, t_array = [],[]
+        for i = 1:length(c_string)
+            push!(c_array,parse(Float64,c_string[i]))
+        end
+        for i = 1:length(t_string)
+            push!(t_array,parse(Float64,t_string[i]))
+        end
+        if haskey(dss_obj, "npts")
+            npts = parse(Int64,dss_obj["npts"])
+            if (length(c_array) != npts) || (length(t_array) != npts)
+                if length(c_array) > npts
+                    @warn "c_array is longer than the npts. Truncating array."
+                    cut_points = length(c_array) - npts
+                    c_array = c_array[cut_points+1:length(c_array)]
+                end
+                if length(t_array) > npts
+                    @warn "t_array is longer than the npts. Truncating array."
+                    cut_points = length(t_array) - npts
+                    t_array = t_array[cut_points+1:length(t_array)]
+                end
+                if length(t_array) < npts
+                    @warn "t_array is shorter than npts. Adding time values."
+                    t_len = length(t_array)
+                    for i=0:npts - t_len - 1
+                        push!(t_array, t_array[t_len+i]/2)
+                    end
+                end
+                if length(c_array) < npts
+                    @warn "c_array is shorter than npts. Adding current values."
+                    c_len = length(c_array)
+                    (a,b) = _bisection(c_array[c_len],t_array[c_len],c_array[c_len-1],t_array[c_len-1])
+                    for i=1:npts - c_len
+                        push!(c_array, round((a/t_array[c_len+i]+1)^(1/b)))
+                    end
+                end
+            end
+        else
+            if length(c_array) != length(t_array)
+                c_len = length(c_array)
+                t_len = length(t_array)
+                if c_len < t_len
+                    @warn "c_array is shorter than t_array. Adding current values."
+                    c_len = length(c_array)
+                    (a,b) = _bisection(c_array[c_len],t_array[c_len],c_array[c_len-1],t_array[c_len-1])
+                    for i=1:npts - c_len
+                        push!(c_array, round((a/t_array[c_len+i]+1)^(1/b)))
+                    end
+                else
+                    @warn "t_array is shorter than c_array. Adding time values."
+                    for i=0:c_len - t_len - 1
+                        push!(t_array, t_array[t_len+i]/2)
+                    end
+                end
+            end
+        end
+        add_curve(data_eng, id, t_array, c_array)
+    end  
 end
