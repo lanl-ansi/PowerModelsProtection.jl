fe8638df94f72124918ebcee4f6b66aca842fcde
David M Fobes <dfobes@lanl.gov>
2021-11-17 13:40:51 -0700

    FIX: whitespace

--- src/io/opendss.jl
+++ src/io/opendss.jl
@@ -10,7 +10,7 @@ data model adding some extra data transformations specific to fault studies.
 """
 function parse_opendss(file::String; method::Union{String,Missing}=missing, kwargs...)
     pm_data = _PMD.parse_file(file; dss2eng_extensions=[_dss2eng_solar_dynamics!, _dss2eng_gen_dynamics!,_dss2eng_curve!,_dss2eng_fuse!,_dss2eng_ct!,_dss2eng_relay!], kwargs...)
-    
+
     if ismissing(method)
         pm_data["method"] = "PMD"
     else
