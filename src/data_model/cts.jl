560d78465dcf7c0508fae19a986a65fdf2df0659
David M Fobes <dfobes@lanl.gov>
2022-03-14 15:21:01 -0600

    REF: move data functions to data_model

--- src/data_model/cts.jl
+++ src/data_model/cts.jl
@@ -1,9 +1,14 @@
 """
-Function to add a current transformer to circuit dictionary.
+    add_ct(data::Dict{String,Any}, element::String, id::String, n_p::Number, n_s::Number;kwargs...)
 
-Inputs(in order):
-
-Circuit dictionary, monitored element, name of fuse, primary and secondary turns. Turns entered seperately and stored as a vector.
+Function to add current transformer to circuit.
+Inputs 4 if adding first CT, 5 otherwise:
+    (1) data(Dictionary): Result from parse_file(). Circuit information
+    (2) element(String): Element or line that CT is being added to
+    (3) id(String): Optional. For multiple CT on the same line. If not used overwrites previously defined CT1
+    (4) n_p(Number): Primary . Would be the number of  of the relay side of transformer
+    (5) n_s(Number): Secondary . Number of  on line side
+    (6) kwargs: Any other information user wants to add. Not used by anything.
 """
 function add_ct(data::Dict{String,Any}, element::Union{String,SubString{String}}, id::Union{String,SubString{String}}, n_p::Number, n_s::Number;kwargs...)
     if !haskey(data, "protection")
